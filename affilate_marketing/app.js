89f73a541b67c87b89022daf769cde07a8d2bd7e
AJEET KUMAR <84391822+Ajeetkumar-spirit@users.noreply.github.com>
2024-09-28 22:18:41 +0530

    Add files via upload

---
+++ affilate_marketing/app.js
@@ -0,0 +1,108 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Bronze Package',
+        image: "bronze.jpg",
+        price: 1899
+    },
+    {
+        id: 2,
+        name: 'Silver Package',
+        image: 'silver.png',
+        price: 3499
+    },
+    {
+        id: 3,
+        name: 'Gold Package',
+        image: 'goldd.jpg',
+        price: 6999
+    },
+    {
+        id: 4,
+        name: 'Platinum Package',
+        image: 'gol.jpg',
+        price: 12999
+    },
+    {
+        id: 5,
+        name: 'Diamond package',
+        image: 'diamond.png',
+        price: 19999
+    },
+    {
+        id: 6,
+        name: 'Conqueror package',
+        image: 'dimond.png',
+        price: 22999
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        listCards[key] = products[key];
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    console.log(key, quantity);
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
---
+++ affilate_marketing/register.js
@@ -0,0 +1,194 @@
+let mail = document.querySelector('.mail');
+let nme = document.querySelector('.name');
+let phn = document.querySelector('.phn');
+let pass = document.querySelector('.password');
+let otp = document.querySelector('.otp');
+let btn = document.querySelector('.submit');
+let snackbar = document.querySelector('.snackbar');
+let loader = document.querySelector('#loader')
+let main = document.querySelector('main');
+loader.style.visibility="hidden";
+let value = document.querySelector('.value');
+function preventBack() {
+    window.history.forward(); 
+}
+  
+setTimeout("preventBack()", 0);
+  
+window.onunload = function () { null };
+window.history.forward();
+function noBack() {
+    window.history.forward();
+}
+btn.addEventListener('click',()=>{
+    try{
+
+        if(pass.value.length<6){
+
+            snackbar.classList.add('show');
+            value.innerHTML="Password should be contain 6 characters";
+            setTimeout(()=>{
+                snackbar.classList.remove('show');
+               
+            },2500) 
+
+        }
+        else{
+
+            if(mail.value == "" && phn.value == "" ){
+                snackbar.classList.add("show");
+                value.innerHTML="Please Enter credentials";
+                setTimeout(()=>{
+                    snackbar.classList.remove('show')
+                },3000)
+            }
+
+            else{
+                let re =/@/i;
+                
+                let result = re.test(mail.value);
+             
+                if(result){
+                    main.style.visibility="hidden";
+        document.body.style.background = "black";
+    loader.style.visibility="visible";
+                let tm = new Date();
+        let date = tm.getDate();
+        let month = tm.getMonth();
+        let yaer = tm.getFullYear();
+        let hr = tm.getHours(); 
+        let min = tm.getMinutes();
+        let sec = tm.getSeconds();
+      
+        let device1 =this.navigator.appCodeName+this.navigator.appName+this.navigator.appVersion+" "+this.navigator.platform;
+        
+                let obj = {
+                    Name:nme.value,
+                    email:mail.value,
+                    Phone:phn.value,
+                    Password:pass.value,
+                    time:`${date}-${month}-${yaer} :: ${hr}:${min}:${sec}`,
+                    device:device1
+
+                }
+        
+              if(btn.innerHTML == "Get-Otp"){  
+                fetch('http://localhost:9000/api/v1/register',{
+                    method:"POST",
+                    body: JSON.stringify(obj),
+                    headers: { 'Content-Type': 'application/json' }
+                })
+                .then(resp=>resp.json())
+                .then(json=>{
+
+                    console.log(json);
+                    if(json.status == "success"){
+                        main.style.visibility="visible";
+                        document.body.style.background = "white";
+                    loader.style.visibility="hidden";
+                        btn.innerHTML = "Submit";
+                        snackbar.classList.add("show");
+                        value.innerHTML=" Otp sends successfully";
+                        setTimeout(()=>{
+                            snackbar.classList.remove('show')
+                            otp.readOnly = false;
+                            mail.readOnly=true;
+                            pass.readOnly=true;
+                            phn.readOnly=true;
+                            nme.readOnly=true;
+                            window.localStorage.setItem('name_affiliate',`${json.User.Name}`)
+                            otp.value= `${json.User.otp}`;
+                        },4000)
+                      
+                        
+                        
+
+                    }
+                    else{
+                        main.style.visibility="visible";
+                        document.body.style.background = "white";
+                    loader.style.visibility="hidden";
+                        snackbar.classList.add("show");
+                        value.innerHTML=json.Error;
+                        setTimeout(()=>{
+                            snackbar.classList.remove('show')
+                        },3000)
+
+                    }
+
+                })
+            }
+
+            else{
+
+                main.style.visibility="hidden";
+                document.body.style.background = "black";
+            loader.style.visibility="visible";
+
+                let obj = {
+                    email:mail.value,
+                    otp:otp.value
+                }
+
+
+                fetch('http://localhost:9000/api/v1/verify',{
+                    method:"POST",
+                    body: JSON.stringify(obj),
+                    headers: { 'Content-Type': 'application/json' }
+                })
+
+                .then(resp=>resp.json())
+                .then(json=>{
+                    console.log(json);
+                    if(json.status == "success"){
+                        main.style.visibility="visible";
+                        document.body.style.background = "white";
+                    loader.style.visibility="hidden";
+                        snackbar.classList.add("show");
+                        value.innerHTML=json.status;
+                        setTimeout(()=>{
+                            snackbar.classList.remove('show')
+                            window.location.href="home-user_learnearn-affiliate-marketing_static.html"
+                        },1000)
+                    }
+                    else{
+                        main.style.visibility="visible";
+                        document.body.style.background = "white";
+                    loader.style.visibility="hidden";
+                        snackbar.classList.add("show");
+                        value.innerHTML=json.msg;
+                        setTimeout(()=>{
+                            snackbar.classList.remove('show')
+                        },3000)
+                    }
+                })
+
+
+
+            }
+
+               
+
+
+            }
+            else{
+                
+                snackbar.classList.add('show');
+                      value.innerHTML="Please Enter valid Credentials";
+                      setTimeout(()=>{
+                          snackbar.classList.remove('show');
+                      },2000) 
+            }
+        }
+      
+        }
+        
+
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+
+       
